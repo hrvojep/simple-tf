fcf5431526f922da48bed1bc6eafa8f71fb6b954
hrvoje pejcinovic <h@hrvojes-mbp.lan>
2021-04-28 10:49:22 +1000

    first commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "ap-southeast-2"
+}
+
+
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-01235362b98157fe1"
+  instance_type = "t2.micro"
+  count = 1
+  tags = {
+    Name = "H-test"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// resource "aws_instance" "app_server" {
+//   ami           = "ami-01235362b98157fe1"
+//   instance_type = "t2.micro"
+//   count = 1
+//   user_data = <<-EOF
+//               #!/bin/bash
+//               echo "Hello, Wendesday World." > index.html
+//               nohup busybox httpd -f -p 8080 &
+//               EOF
+
+
+//   tags = {
+//     Name = "H-test"
+//   }
+// }
+
+// output "instance_ip_addr" {
+//   value = aws_instance.app_server.*.public_ip
+// }
