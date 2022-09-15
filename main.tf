8663de565b46885ed4efb4cc793f526deaefd51d
H <hrvojep@yahoo.com>
2022-09-15 19:16:09 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 4.16"
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
